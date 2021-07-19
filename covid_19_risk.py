7ad30827a03c468d8ed04d17e38124e845a42e9a
kaya9163 <87029680+kaya9163@users.noreply.github.com>
2021-07-19 23:45:36 +0200

    Create covid_19_risk.py
    covid_19

---
+++ covid_19_risk.py
@@ -0,0 +1,7 @@
+age = input("Are you a cigarette addict older than 75 years old? (True or False) : ")
+chronic = input("Do you have a severe chronic disease? (True or False) : ")
+immune = input("Is your immune system too weak? (True or False) : ")
+if (age == "True") or (chronic == "True") or (immune == "True") :
+  print("there is a risk of death")
+else :
+  print("there is not a risk of death")  
