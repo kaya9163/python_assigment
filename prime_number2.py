70db6caeec4f0593cb2ea8f39eb4674f87589b3f
kaya9163 <87029680+kaya9163@users.noreply.github.com>
2021-08-03 16:12:45 +0200

    Update prime_number2.py
    fix the bugs

--- prime_number2.py
+++ prime_number2.py
@@ -1,7 +1,7 @@
 number = int(input("Please Enter A Number: "))
 if number > 1:
     for i in range (2, number):
-        if num%i == 0:
+        if number%i == 0:
             print(number, "is not a prime number")
         else:
             print(number, "is a prime number")
