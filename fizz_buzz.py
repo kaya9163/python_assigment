b495e7f6e669f1bf4be86826c51fbd6dc537e31a
kaya9163 <87029680+kaya9163@users.noreply.github.com>
2021-08-15 11:25:13 +0200

    fizz_buzz modified.

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,13 @@
+"""
+Task : Print the Fizz Buzz numbers.
+
+Fizz Buzz is a famous code challenge used in interviews to test basic programming skills. It's time to write your own implementation.
+Print numbers from 1 to 100 inclusively following these instructions:
+if a number is multiple of 3, print "Fizz" instead of this number,
+if a number is multiple of 5, print "Buzz" instead of this number,
+for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+print the rest of the numbers unchanged.
+Output each value on a separate line."""
 
 def hesapla(n, y):
     for i in range (n, y+1):
@@ -9,4 +19,4 @@ def hesapla(n, y):
             print("fizzbuzz")
         else:
             print(i)
-hesapla(1,50)
+hesapla(1,100)
